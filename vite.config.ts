ed68661f38f433078c9f322d9b08b618626b04ec
d-naumov <nd76@gmx.de>
2023-11-12 15:47:58 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Project_front/",
   server: {
     open: true,
   },
